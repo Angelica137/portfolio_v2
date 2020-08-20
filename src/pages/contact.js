a52f07e494bb370778521ed15bb4946b7bce9d07
Angelica <Angelica137@users.noreply.github.com>
2020-08-21 00:51:00 +0100

    add icon button to landing

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,8 +7,18 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="Angelica Beristain: contact" />
     <h1>Let's work together 👩‍💻</h1>
-    <p>You can find me on all of these places</p>
+    <p>You can reach me on</p>
   </Layout>
+  //<div
+  //</Layout>    style={
+  //</Layout>      "display: flex; flex-direction: row; justify-content: space-between; width: 200px;"
+  //</Layout>    }
+  //</Layout>  >
+  //</Layout>    <p>Linkedin</p>
+  //</Layout>    <p>Github</p>
+  //</Layout>    <p>mobile</p>
+  //</Layout>  </div>
+  //</Layout>
 );
 
 export default NotFoundPage;
